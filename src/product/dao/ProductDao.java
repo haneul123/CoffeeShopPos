d06b527ebf438ab9bb9978a3c8e4b5df5cfd1d89
haneul123 <ww4020@naver.com>
2016-09-06 18:52:01 +0900

    product,user,login

--- src/product/dao/ProductDao.java
+++ src/product/dao/ProductDao.java
@@ -1,5 +1,26 @@
 package product.dao;
 
 public class ProductDao {
+	
+	
+	public void add(){
+		
+	}
 
+	
+	public void search(){
+		
+	}
+	
+	
+	public void delete(){
+		
+	}
+	
+	
+	public void update(){
+		
+	}
+	
+	
 }
--- src/user/vo/User.java
+++ src/user/vo/User.java
@@ -1,5 +1,79 @@
 package user.vo;
 
 public class User {
+	
+	private String number;
+	private String id;
+	private String name;
+	private String loginTime;
+	private int couponCount;
+	
+	
+	public User(){
+		
+	}
+	
+	
+	public User(String id, String name, String loginTime, int couponCount){
+		
+		this.number = number;
+		this.id = id;
+		this.name = name;
+		this.loginTime = loginTime;
+		this.couponCount = couponCount;
+		
+	}
+
+
+	public String getNumber() {
+		return number;
+	}
+
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getLoginTime() {
+		return loginTime;
+	}
+
+
+	public void setLoginTime(String loginTime) {
+		this.loginTime = loginTime;
+	}
+
+
+	public int getCouponCount() {
+		return couponCount;
+	}
+
+
+	public void setCouponCount(int couponCount) {
+		this.couponCount = couponCount;
+	}
+	
+	
 
 }
