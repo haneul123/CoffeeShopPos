98e854bc836bb3670a6d62df788d8798e5b0df14
WonJae Choi <choiwj1012@hanmail.net>
2016-09-27 14:29:45 +0900

    [add] #CSP001 관리자 메뉴 생성중

--- src/admin/controller/AdminController.java
+++ src/admin/controller/AdminController.java
@@ -2,82 +2,94 @@ package admin.controller;
 
 import admin.dao.AdminDao;
 import admin.view.AdminMainMenu;
+import admin.view.AdminManagementView;
 import admin.view.AdminSignupView;
 import admin.vo.Admin;
 import mainController.MainController;
 
 public class AdminController {
 
-	
+
 	// variable
 	private AdminDao adminDao;
-	
-	
+
+
 	// constructor
 	public AdminController() {
-	
+
 		this.adminDao = new AdminDao();
-		
+
 	}
-	
-		
+
+
 	// method
 	// 관리자 회원가입 정보 요청
 	public void requestSignUpInfo(){
-		
+
 		AdminSignupView adminSignupView = new AdminSignupView();
 		adminSignupView.adminSignupView();
-		
+
 	}
-	
-	
+
+
 	// 관리자 회원가입 요청
 	public void requestSignUp(Admin newAdmin){
-	
+
 		boolean success = adminDao.signUp(newAdmin);
-		
+
 		if(success){
-		
+
 			requestMainMenuView();
-			
+
 		} else {
-			
+
 			System.out.println("회원가입에 실패하였습니다");
 			MainController.mainMenuView();
-			
+
 		}
-		
+
 	}
-	
-	
+
+
 	// 관리자 메인메뉴 뷰 요청
 	public void requestMainMenuView(){
-		
+
 		AdminMainMenu adminMainMenu = new AdminMainMenu();
 		adminMainMenu.adminMainMenu();
-				
+
 	}
-	
-	
-	
-	
+
+
+	// 직원관리 메뉴 뷰 출력요청
+	public void requestAdminManagementView() {
+
+		AdminManagementView adminManagementView = new AdminManagementView();
+		adminManagementView.adminManagemnetView();
+
+	}
+
+
 	// 관리자 목록보기
 	public void requestListView(){
+
+		
 		
 	}
-	
-	
+
+
 	// 관리자 수정하기
 	public void requestUpdate(){
-		
+
+
 		
 	}
-	
-	
+
+
 	// 관리자 삭제하기
 	public void requestDelete(){
+
 		
-		
+
 	}
-		
+
 }
