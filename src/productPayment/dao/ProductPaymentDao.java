f304d79a9909f1fa7d8a9cf6f8a5b85059090c87
wonjae choi <choiwj1012@hanmail.net>
2016-10-02 02:20:18 +0900

    [mod]#CSP003-1 상품 주문시 원재료가 감소되는 기능 테스트중

--- src/productPayment/dao/ProductPaymentDao.java
+++ src/productPayment/dao/ProductPaymentDao.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import mainController.MainController;
 import productPayment.vo.ProductPayment;
 import productPaymentRepository.ProductOrderRepository;
+import productPaymentRepository.UseIngredientAmount;
 
 public class ProductPaymentDao {
 
@@ -19,15 +20,19 @@ public class ProductPaymentDao {
 		Statement stmt1 = null;
 		Statement stmt2 = null;
 		Statement stmt3 = null;
+		Statement stmt4 = null;
 		PreparedStatement pstmt1 = null;
 		PreparedStatement pstmt2 = null;
 		PreparedStatement pstmt3 = null;
+		PreparedStatement pstmt4 = null;
 		ResultSet rs1 = null;
 		ResultSet rs2 = null;
 		ResultSet rs3 = null;
+		ResultSet rs4 = null;
 		int freeCoupon = 0;
 		int remainCoupon = 0;
 		ArrayList<ProductPayment> paymentList = new ArrayList<ProductPayment>();
+		ArrayList<UseIngredientAmount> useAmountList = new ArrayList<UseIngredientAmount>();
 
 		try {
 
@@ -56,7 +61,7 @@ public class ProductPaymentDao {
 			// 가져온 주문 데이터를 결제 데이터에 저장하기
 			for(int i = 0; i<paymentList.size(); i++){
 
-				sql = "insert into product_pay_list values(product_pay_number_seq.nextval, ?, ?)";
+				sql = "insert into product_pay_list values(product_pay_number_seq.nextval, ?, ?, 1)";
 				pstmt1 = MainController.getDbController().getConnection().prepareStatement(sql);
 				pstmt1.setInt(1, paymentList.get(i).getProductOrderNumber());
 				pstmt1.setInt(2, paymentList.get(i).getPaymentNumber());
@@ -64,8 +69,7 @@ public class ProductPaymentDao {
 
 			}
 
-			isSuccess = true;
-
+			
 			// 쿠폰처리 (비회원은 쿠폰수를 계산하지 않는다)
 			if(paymentList.get(0).getUserNumber() != 1){
 
@@ -98,7 +102,7 @@ public class ProductPaymentDao {
 				sql = "select (pl.product_price * pol.ORDER_COUNT) as totalPrice, ";
 				sql += "((pl.product_price * pol.ORDER_COUNT) - (pl.product_price * ?)) as realPrice ";
 				sql	+= "from product_list pl, product_order_list pol ";
-				sql	+= "where pl.product_number = pol.product_number";
+				sql	+= "where pl.product_number = pol.product_number and pol.isAgreePaid = 1";
 				pstmt3 = MainController.getDbController().getConnection().prepareStatement(sql);
 				pstmt3.setInt(1, freeCoupon);
 				rs3 = pstmt3.executeQuery();
@@ -112,19 +116,50 @@ public class ProductPaymentDao {
 
 			}
 
-			// 완료된 주문에 해당하는 원재료 감소 처리
 			
-
-
-
+			// 완료된 주문에 해당하는 원재료 감소 처리를 위한 정보 수집
+			sql = "select pui.product_number, pui.ingredient_number, (pui.use_amount * oc.order_count) "; 
+			sql += "from product_use_ingredient pui, order_count oc ";	
+			sql += "where pui.product_number = oc.PRODUCT_NUMBER";
+			stmt4 = MainController.getDbController().getConnection().createStatement();
+			rs4 = stmt4.executeQuery(sql);
+			while(rs4.next()){
+				
+				UseIngredientAmount useAmount = new UseIngredientAmount();
+				useAmount.setProductNumber(rs4.getInt(1));
+				useAmount.setIngredientNumber(rs4.getInt(2));
+				useAmount.setTotalAmount(rs4.getInt(3));
+				useAmountList.add(useAmount);
+				
+			}
+			
+			
+			// 원재료 감소 처리
+			for(int i=0; i<useAmountList.size(); i++){
+				
+				sql =  "update ingredient_list ";
+				sql += "set ingredient_inventory = (ingredient_inventory - ?) ";
+				sql += "where ingredient_number = ?";
+				pstmt4 = MainController.getDbController().getConnection().prepareStatement(sql);
+				pstmt4.setInt(1, useAmountList.get(i).getTotalAmount());
+				pstmt4.setInt(2, useAmountList.get(i).getIngredientNumber());
+				pstmt4.executeUpdate();
+				
+			}
+			 				
+			isSuccess = true;
+			
 		}catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
+			if(pstmt4 != null){try {pstmt4.close();} catch (SQLException e) {e.printStackTrace();}}
+			if(rs4 != null){try {rs4.close();} catch (SQLException e) {e.printStackTrace();}}
+			if(stmt4 != null){try {stmt4.close();} catch (SQLException e) {e.printStackTrace();}}
 			if(rs3 != null){try {rs3.close();} catch (SQLException e) {e.printStackTrace();}}
 			if(pstmt3 != null){try {pstmt3.close();} catch (SQLException e) {e.printStackTrace();}}
 			if(pstmt2 != null){try {pstmt2.close();} catch (SQLException e) {e.printStackTrace();}}
 			if(rs2 != null){try {rs2.close();} catch (SQLException e) {e.printStackTrace();}}
-			if(stmt3 != null){try {stmt2.close();} catch (SQLException e) {e.printStackTrace();}}
+			if(stmt3 != null){try {stmt3.close();} catch (SQLException e) {e.printStackTrace();}}
 			if(pstmt1 != null){try {pstmt1.close();} catch (SQLException e) {e.printStackTrace();}}
 			if(stmt2 != null){try {stmt2.close();} catch (SQLException e) {e.printStackTrace();}}
 			if(rs1 != null){try {rs1.close();} catch (SQLException e) {e.printStackTrace();}}
---
+++ src/productPaymentRepository/UseIngredientAmount.java
@@ -0,0 +1,40 @@
+package productPaymentRepository;
+
+public class UseIngredientAmount {
+	
+	// variable
+	private int ingredientNumber = 0;
+	private int productNumber = 0;
+	private int totalAmount = 0;
+	
+	// constructor
+	public UseIngredientAmount() {
+		
+	}
+	
+	// getter and setter
+	public int getIngredientNumber() {
+		return ingredientNumber;
+	}
+
+	public void setIngredientNumber(int ingredientNumber) {
+		this.ingredientNumber = ingredientNumber;
+	}
+
+	public int getProductNumber() {
+		return productNumber;
+	}
+
+	public void setProductNumber(int productNumber) {
+		this.productNumber = productNumber;
+	}
+
+	public int getTotalAmount() {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(int totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+		
+}
