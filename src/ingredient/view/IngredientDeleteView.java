6c14cf5c9eb3e6b7fcb9da91f049e3dbe169715e
haneul123 <ww4020@naver.com>
2016-10-01 19:43:02 +0900

    [add] ingredint/list/update

--- src/ingredient/view/IngredientDeleteView.java
+++ src/ingredient/view/IngredientDeleteView.java
@@ -3,37 +3,28 @@ package ingredient.view;
 import java.util.Scanner;
 
 import ingredient.vo.Ingredient;
+import mainController.MainController;
 import mainView.AlertView;
 
 public class IngredientDeleteView {
 
 	private Scanner keyboard;
-	
+
 	public IngredientDeleteView() {
-		
+
 		keyboard = new Scanner(System.in);
 	}
 
-	public Ingredient deleteIngredient() {
-		
-		new AlertView().alert("[원재료 삭제 모드]");
-		System.out.print("삭제할 제품 이름 입력 : ");
-		String deleteIngredientName = keyboard.next();
-		
-		Ingredient deleteIngredientGetNmae = new Ingredient(deleteIngredientName);
-		
-		return deleteIngredientGetNmae;
-		
-	}
-	
-	public int deleteIngredientNum() {
-		
+
+	public void deleteIngredientNum() {
+
 		System.out.print("삭제 제품번호 입력 : ");
-		
+
 		int deleteIngredientGetNum = keyboard.nextInt();
-		
-		return deleteIngredientGetNum;
+
+		MainController.getIngredientController().requestDeleteIngredient(deleteIngredientGetNum);
+
 	}
-	
-	
+
+
 }
